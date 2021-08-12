a3001003044e5c4a23afa7f93f1aeb7542524410
Ankit <kumarankit@protonmail.com>
2021-08-12 23:23:44 +0530

    Adds plus minus on product page

--- functions.php
+++ functions.php
@@ -212,3 +212,50 @@ function handy_remove_product_page_sku( $enabled ) {
     }
     return $enabled;
 }
+
+/* Move tabs to Summary area */
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60 );
+
+// Show plus minus buttons
+add_action( 'woocommerce_after_quantity_input_field', 'handy_display_quantity_plus' );
+  function handy_display_quantity_plus() {
+   echo '<button type="button" class="qty-button plus" >+</button>';
+}
+
+add_action( 'woocommerce_before_quantity_input_field', 'handy_display_quantity_minus' );
+function handy_display_quantity_minus() {
+   echo '<button type="button" class="qty-button minus" >-</button>';
+}
+
+// Trigger update quantity script
+// add_action( 'wp_footer', 'handy_add_cart_quantity_plus_minus' );
+// function handy_add_cart_quantity_plus_minus() {
+//    if ( ! is_product() && ! is_cart() ) return;
+    
+//    wc_enqueue_js("     
+//       $('form.cart,form.woocommerce-cart-form').on( 'click', 'button.plus, button.minus', function() {
+  
+//          var qty = $( this ).parent( '.quantity' ).find( '.qty' );
+//          var val = parseFloat(qty.val());
+//          var max = parseFloat(qty.attr( 'max' ));
+//          var min = parseFloat(qty.attr( 'min' ));
+//          var step = parseFloat(qty.attr( 'step' ));
+ 
+//          if ( $( this ).is( '.plus' ) ) {
+//             if ( max && ( max <= val ) ) {
+//                qty.val( max );
+//             } else {
+//                qty.val( val + step );
+//             }
+//          } else {
+//             if ( min && ( min >= val ) ) {
+//                qty.val( min );
+//             } else if ( val > 1 ) {
+//                qty.val( val - step );
+//             }
+//          }
+ 
+//       }); 
+//    " );
+// }
