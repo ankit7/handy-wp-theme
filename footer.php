ecd0a69d26d3accd9e1459b55dbfac3f0a80ffc5
Ankit <kumarankit@protonmail.com>
2021-08-13 13:58:18 +0530

    footer bootstrap

--- footer.php
+++ footer.php
@@ -1,95 +1,102 @@
 <div class="footer">
 	<div class="footer-content">
-		<div class="left">
-			<img src="" alt="Logo">
-			<strong>Do you want 7% off?</strong>
-			<p>
-				Sign up for our newsletter and get a 7% discount code (Does not
-				apply to sales items)
-			</p>
-			<label>
-				<input placeholder="Your email adress " type="email" name="email">
-				<img class="arrow" src="<?php echo get_template_directory_uri(); ?>/inc/assets/img/right-arrow.png" alt="">
-			</label>
-			<p>© 2021 Handy ™ | All rights reserved.</p>
-		</div>
-		<div class="right">
-			<div class="upper">
-				<div>
-					<strong>Site</strong>
-					<a href="/">Home</a>
-					<a href="/shop">Shop</a>
-					<a href="/journal">Journal</a>
-					<a href="/connect">Connect</a>
-				</div>
-				<div>
-					<strong>Other</strong>
-					<a href="/careers">Carrers</a>
-					<a href="/how-to">How to guides</a>
-					<a href="/handy-feeling">Handy Feeling</a>
-					<a href="/retailers">Retailers</a>
-					<a href="/user-manual">User Manual</a>
-				</div>
-				<div>
-					<strong>Post address</strong>
-					<p>
-						Sweet Tech AS <br>
-						c/o Mesh Community <br>Mollergata 6 <br>
-						0179 Oslo
-					</p>
-					<strong>Contact</strong>
-					<p>contact@thehandy.com</p>
-				</div>
-			</div>
-			<div class="bottom">
-				<strong>Follow us</strong>
-				<div>
-					<a href="https://www.facebook.com">Facebook</a>
-					<a href="https://www.youtube.com">Youtube</a>
-					<a href="https://www.instagram.com">Instagram</a>
-					<a href="https://www.reddit.com">Reddit</a>
-					<a href="https://www.twitter.com">Twitter</a>
-					<a href="https://www.discrord.com">Discord</a>
-				</div>
-			</div>
-		</div>
-		<div class="right-mobile">
-			<div class="left">
-				<strong>Site</strong>
-				<a href="/">Home</a>
-				<a href="/shop">Shop</a>
-				<a href="/journal">Journal</a>
-				<a href="/connect">Connect</a>
-				<strong>Follow us</strong>
-				<a href="https://www.facebook.com">Facebook</a>
-				<a href="https://www.youtube.com">Youtube</a>
-				<a href="https://www.instagram.com">Instagram</a>
-				<a href="https://www.reddit.com">Reddit</a>
-				<a href="https://www.twitter.com">Twitter</a>
-				<a href="https://www.discrord.com">Discord</a>
-				<strong>Post address</strong>
+		<div class="row">
+			<div class="col-sm-6">
+				<p><img src="" alt="Logo"></p>
+				<p><strong>Do you want 7% off?</strong></p>
 				<p>
-					Sweet Tech AS <br>
-					c/o Mesh Community <br>
-					Mollergata 6 <br>
-					0179 Oslo
+					Sign up for our newsletter and get a 7% discount code (Does not
+					apply to sales items)
 				</p>
-			</div>
+				<form action="" class="newsletter">
+					<label>
+						<input placeholder="Your email adress " type="email" name="email">
+						<img class="arrow" src="<?php echo get_template_directory_uri(); ?>/inc/assets/img/right-arrow.png" alt="">
+					</label>
+				</form>
+				<p>© 2021 Handy ™ | All rights reserved.</p>
 
-			<div class="right">
-				<strong>Diverse</strong>
-				<a href="/careers">Carrers</a>
-				<a href="/how-to">How to guides</a>
-				<a href="/handy-feeling">Handy Feeling</a>
-				<a href="/affiliate-program">Affiliate program</a>
-				<a href="/retailers">Retailers</a>
-				<a href="/user-manual">User Manual</a>
-				<strong>Community</strong>
-				<p>
-					Visit our community on <a href="https://www.reddit.com">Reddit</a>
-				</p>
-				<strong>Contact</strong>
-				<p>Email us <a href="mailto:contact@thehandy.com">here</a></p>
+			</div>
+			<div class="col-sm-6">
+				<div class="row">
+					<div class="col-sm-4">
+						<strong>Site</strong>
+						<ul class="list-unstyled">
+							<li><a href="/">Home</a></li>
+							<li><a href="/shop">Shop</a></li>
+							<li><a href="/journal">Journal</a></li>
+							<li><a href="/connect">Connect</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-4">
+						<strong>Other</strong>
+						<ul class="list-unstyled">
+							<li><a href="/careers">Carrers</a></li>
+							<li><a href="/how-to">How to guides</a></li>
+							<li><a href="/handy-feeling">Handy Feeling</a></li>
+							<li><a href="/retailers">Retailers</a></li>
+							<li><a href="/user-manual">User Manual</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-4">
+						<strong>Post address</strong>
+						<p>
+							Sweet Tech AS <br>
+							c/o Mesh Community <br>Mollergata 6 <br>
+							0179 Oslo
+						</p>
+						<strong>Contact</strong>
+						<p>contact@thehandy.com</p>
+					</div>
+				</div>
+				<p><strong>Follow us</strong></p>
+				<ul class="list-unstyled">
+					<li class="list-inline-item"><a href="https://www.facebook.com">Facebook</a></li>
+					<li class="list-inline-item"><a href="https://www.youtube.com">Youtube</a></li>
+					<li class="list-inline-item"><a href="https://www.instagram.com">Instagram</a></li>
+					<li class="list-inline-item"><a href="https://www.reddit.com">Reddit</a></li>
+					<li class="list-inline-item"><a href="https://www.twitter.com">Twitter</a></li>
+					<li class="list-inline-item"><a href="https://www.discrord.com">Discord</a></li>
+				</ul>
+				<div class="right-mobile">
+					<div class="left">
+						<strong>Site</strong>
+						<a href="/">Home</a>
+						<a href="/shop">Shop</a>
+						<a href="/journal">Journal</a>
+						<a href="/connect">Connect</a>
+						<strong>Follow us</strong>
+						<a href="https://www.facebook.com">Facebook</a>
+						<a href="https://www.youtube.com">Youtube</a>
+						<a href="https://www.instagram.com">Instagram</a>
+						<a href="https://www.reddit.com">Reddit</a>
+						<a href="https://www.twitter.com">Twitter</a>
+						<a href="https://www.discrord.com">Discord</a>
+						<strong>Post address</strong>
+						<p>
+							Sweet Tech AS <br>
+							c/o Mesh Community <br>
+							Mollergata 6 <br>
+							0179 Oslo
+						</p>
+					</div>
+
+					<div class="right">
+						<strong>Diverse</strong>
+						<a href="/careers">Carrers</a>
+						<a href="/how-to">How to guides</a>
+						<a href="/handy-feeling">Handy Feeling</a>
+						<a href="/affiliate-program">Affiliate program</a>
+						<a href="/retailers">Retailers</a>
+						<a href="/user-manual">User Manual</a>
+						<strong>Community</strong>
+						<p>
+							Visit our community on <a href="https://www.reddit.com">Reddit</a>
+						</p>
+						<strong>Contact</strong>
+						<p>Email us <a href="mailto:contact@thehandy.com">here</a></p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
