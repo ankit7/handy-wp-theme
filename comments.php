55435ab428b598c29fe4b9055f2772e6d22a6967
Ankit <kumarankit@protonmail.com>
2021-08-13 23:52:37 +0530

    Expands container width

--- comments.php
+++ comments.php
@@ -99,10 +99,6 @@ if ( post_password_required() ) {
 
         'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
 
-        'comment_notes_after' => '<p class="form-allowed-tags">' .
-            __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', 'handy' ) .
-            '</p><div class="alert alert-info">' . allowed_tags() . '</div>'
-
         // So, that was the needed stuff to have bootstrap basic styles for the form elements and buttons
 
         // Basically you can edit everything here!
