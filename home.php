3cbd5724430daea8924506edd2f25e787d001dea
Ankit <kumarankit@protonmail.com>
2021-08-24 11:09:11 +0530

    Journal page mis changes

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
             <h2 class="light-white">The Handy Diary</h2>
             <p>We talk about sex, life and pleasure. Let's transform the sexual topics to cool opened talk.</p>
           </div>
-          <div class="card bg-transparent border-bottom mt-5">
+          <div class="card bg-transparent border-bottom mt-5 mb-5">
             <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/img/journal/journal1.jpg" alt="" class="card-img-top">
             <div class="card-body p-0">
               <h5 class="card-title light-white">The big problem of the small aggressions </h5>
@@ -20,13 +20,14 @@
               <p class="card-text">In the latest instalment of our mini series on inclusivity in the workplace, our Head of HR and Compliance Isabel Ros Ruiz writes...</p>
             </div>
           </div>
+          <a href="#" class="btn btn-primary btn-xl">Read more from this section</a>
         </div>
         <div class="col-lg-6">
         <div class="border-bottom mb-3">
           <h2 class="light-white">The Handy News</h2>
-          <p>The Handy is alive and constantly evolves in front of your eyes. See what new, exciting features we have for you today!</p>
+          <p>The Handy is alive and constantly evolves in front of your eyes. See whats new..</p>
         </div>
-        <div class="card bg-transparent border-bottom mt-5">
+        <div class="card bg-transparent border-bottom mt-5 mb-5">
           <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/img/journal/journal2.jpg" alt="" class="card-img-top">
           <div class="card-body p-0">
             <h5 class="card-title light-white">New patch release: Firmware 3.1</h5>
@@ -34,6 +35,7 @@
             <p class="card-text">Dear Handy Community, As you know, we’ve been in the process of gradually rolling out FW3. Unfortunately, there were some...</p>
           </div>
         </div>
+        <a href="#" class="btn btn-secondary btn-xl">Read more from this section</a>
         </div>
       </div>
     </div>
@@ -43,13 +45,13 @@
 <div id="journal-content">
   <div class="container">
     <div class="row mb-4">
-      <div class="col-lg-4">
-        <a href="#" class="btn btn-primary btn-block">All Articles</a>
+      <div class="col-lg-4 mb-3">
+        <a href="#" class="btn btn-light btn-block">All Articles</a>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-4 mb-3">
         <a href="#" class="btn btn-primary btn-block">The Handy Diary articles</a>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-4 mb-3">
         <a href="#" class="btn btn-primary btn-block">The Handy News articles</a>
       </div>
     </div>
@@ -115,7 +117,9 @@
         </div>
       </div>
     </div>
-    <button class="load-more">Load more articles</button>
+    <div class="row justify-content-center mt-5">
+      <a class="btn btn-primary btn-xl">Load more articles</a>
+    </div>
   </div>
 </div>
 
