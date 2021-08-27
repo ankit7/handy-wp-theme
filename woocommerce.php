c417a4233aac8f3bfa0b955480316dc54823f797
Ankit <kumarankit@protonmail.com>
2021-08-27 13:33:59 +0530

    Reviews on Product page

--- woocommerce.php
+++ woocommerce.php
@@ -38,109 +38,115 @@ get_header(); ?>
     <!-- Reviews -->
     <?php if (is_product()): ?>
         <div class="reviews-background">
-            <div class="reviews">
-                <div class="top">
-                <h2 class="text-white">Reviews</h2>
+            <div class="container">
+                <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/inc/assets/css/fontawesome.min.css">
+                <div class="row justify-content-center text-lw">
+                    <div class="col-lg-8">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h2 class="text-white">Reviews</h2>
+                            <a href="/reviews/" class="btn btn-primary">Leave a review</a>
+                        </div>
 
-                <button>Leave a review</button>
+                        <div class="comment">
+                        <div class="first-row">
+                            <h5 class="text-white">Ray Donovan</h5>
+                            <p>14.08.2020</p>
+                        </div>
+
+                        <div class="rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+
+                        <p>
+                            Thank you for such a wonderful product for male masturbation, can't
+                            say enough about it!!! I've been using the handy for almost a year
+                            now, &amp; I've experienced numerous powerful orgasms with it, thank you
+                            so much!!!
+                        </p>
+
+                        <hr>
+                        </div>
+
+                        <div class="comment">
+                        <div class="first-row">
+                            <h5 class="text-white">An anonymous user</h5>
+                            <p>14.08.2020</p>
+                        </div>
+
+                        <div class="rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+
+                        <p>Amazing, worth every penny</p>
+                        </div>
+                        <hr>
+
+                        <div class="comment">
+                        <div class="first-row">
+                            <h5 class="text-white">Ray Donovan</h5>
+                            <p>14.08.2020</p>
+                        </div>
+
+                        <div class="rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+
+                        <p>
+                            Its new to me but it's not much better than anything else. At leat
+                            it can somewhat handle a kinda big dick. Ive had no luck connection
+                            to my wifi yet. Maybe a new phone will help. Maybe it will be
+                            another masturbation machine I throw in the trash.
+                        </p>
+                        </div>
+                        <hr>
+
+                        <div class="comment">
+                        <div class="first-row">
+                            <h5 class="text-white">An anonymous user</h5>
+                            <p>14.08.2020</p>
+                        </div>
+
+                        <div class="rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </div>
+
+                        <p>
+                            I've recently purchased Handy and I'm very happy that I did. It took
+                            me a few uses to figure out how to position the sleeve (how tight
+                            and at what height attach the strap) to get the best experience. It
+                            is worth a note that sleeve can stretch A LOT, which is a really
+                            good thing. I've also tried reverting the sleeve which gave me a
+                            completely different, soft sensation (I usually prefer very soft
+                            sleeves). It feels like getting a very soft blowjob and allows me to
+                            have longer "Handy" sessions. I've also recently ordered a new soft
+                            open-ended sleeve, which looks very promising. I'm definately
+                            waiting for some...
+                        </p>
+                        </div>
+                        <div class="text-center mb-3">
+                            <a href="/reviews/" class="btn btn-primary">Read More</a>
+                        </div>
+
+                        <hr>
+                    </div>
                 </div>
-
-                <div class="comment">
-                <div class="first-row">
-                    <h5 class="text-white">Ray Donovan</h5>
-                    <p>14.08.2020</p>
-                </div>
-
-                <div class="rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                </div>
-
-                <p>
-                    Thank you for such a wonderful product for male masturbation, can't
-                    say enough about it!!! I've been using the handy for almost a year
-                    now, &amp; I've experienced numerous powerful orgasms with it, thank you
-                    so much!!!
-                </p>
-
-                <hr>
-                </div>
-
-                <div class="comment">
-                <div class="first-row">
-                    <h5 class="text-white">An anonymous user</h5>
-                    <p>14.08.2020</p>
-                </div>
-
-                <div class="rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                </div>
-
-                <p>Amazing, worth every penny</p>
-                </div>
-                <hr>
-
-                <div class="comment">
-                <div class="first-row">
-                    <h5 class="text-white">Ray Donovan</h5>
-                    <p>14.08.2020</p>
-                </div>
-
-                <div class="rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                </div>
-
-                <p>
-                    Its new to me but it's not much better than anything else. At leat
-                    it can somewhat handle a kinda big dick. Ive had no luck connection
-                    to my wifi yet. Maybe a new phone will help. Maybe it will be
-                    another masturbation machine I throw in the trash.
-                </p>
-                </div>
-                <hr>
-
-                <div class="comment">
-                <div class="first-row">
-                    <h5 class="text-white">An anonymous user</h5>
-                    <p>14.08.2020</p>
-                </div>
-
-                <div class="rating">
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                </div>
-
-                <p>
-                    I've recently purchased Handy and I'm very happy that I did. It took
-                    me a few uses to figure out how to position the sleeve (how tight
-                    and at what height attach the strap) to get the best experience. It
-                    is worth a note that sleeve can stretch A LOT, which is a really
-                    good thing. I've also tried reverting the sleeve which gave me a
-                    completely different, soft sensation (I usually prefer very soft
-                    sleeves). It feels like getting a very soft blowjob and allows me to
-                    have longer "Handy" sessions. I've also recently ordered a new soft
-                    open-ended sleeve, which looks very promising. I'm definately
-                    waiting for some...
-                </p>
-                </div>
-
-                <button>Read More</button>
-                <hr>
-            </div>
+            </div> 
         </div>
     <?php endif ?>
 <?php
