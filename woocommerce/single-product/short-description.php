0d27d752046d3cde22eba893efd444921d4b050f
Ankit <kumarankit@protonmail.com>
2021-11-03 19:24:17 +0530

    Adds new care/descripton for sleeves

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -32,77 +32,181 @@ if ( ! $short_description ) {
 	<?php echo $short_description; // WPCS: XSS ok. ?>
 	<!-- Sleeves Specific USPs & Specs -->
 	<?php if (is_product() && has_term( 'Sleeve', 'product_cat' )): ?>
-		<ul class="usp my-3 list-unstyled">
-			<li class="mb-2">
-				<img src="/wp-content/themes/handy/inc/assets/img/product/clean.svg" alt="" width="20"
-					class="img-fluid mr-2 mb-2">For water based lubricant only!
-			</li>
-			<li class="mb-2">
-				<img src="/wp-content/themes/handy/inc/assets/img/product/safe.svg" alt="" width="20" class="mr-2 mb-2">100% Safe
-				and Anonymous
-			</li>
-			<li class="mb-2">
-				<img src="/wp-content/themes/handy/inc/assets/img/product/rating.svg" alt="" width="20" class="mr-2 mb-2">4.5
-				Customer Rating
-			</li>
-		</ul>
-		<!-- <div class="accordions">
-			<div class="accordion">
-				<div class="accordion-item">
-					<button id="accordion-button-1" aria-expanded="true"><span class="accordion-title">Specifications</span><span
-							class="icon" aria-hidden="true"></span></button>
-					<div class="accordion-content">
-						<p class="mb-0"><strong>Length 11-12cm</strong></p>
-						<p>*do to the material traites the size might slightlly differ from sleeve to sleeve.</p>
-						<p class="mb-0"><strong>Entrance diameter 15-80mm</strong></p>
-						<p>*do to the material traites the sleeve is extremlly flexible and can strech extensivelly. Note that the
-							Open-Ended Collection sleeves have a wider diameter inside than the entrance one.</p>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Patterns</span><span
-							class="icon" aria-hidden="true"></span></button>
-					<div class="accordion-content">
-						<ul class="specifications">
-							<li>The Handy</li>
-							<li>Power supply</li>
-							<li>Power cable (socket for your country included)</li>
-							<li>TrueGrip sleeve</li>
-							<li>TrueGrip band</li>
-							<li>Manual</li>
-						</ul>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Longevity</span><span
-							class="icon" aria-hidden="true"></span></button>
-					<div class="accordion-content">
-						<ul class="specifications">
-							<li>The Handy</li>
-							<li>Power supply</li>
-							<li>Power cable (socket for your country included)</li>
-							<li>TrueGrip sleeve</li>
-							<li>TrueGrip band</li>
-							<li>Manual</li>
-						</ul>
-					</div>
-				</div>
-				<div class="accordion-item">
-					<button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">Care</span><span
-							class="icon" aria-hidden="true"></span></button>
-					<div class="accordion-content">
-						<ul class="specifications">
-							<li>The Handy</li>
-							<li>Power supply</li>
-							<li>Power cable (socket for your country included)</li>
-							<li>TrueGrip sleeve</li>
-							<li>TrueGrip band</li>
-							<li>Manual</li>
-						</ul>
-					</div>
+	<ul class="usp my-3 list-unstyled">
+		<li class="mb-2">
+			<img src="/wp-content/themes/handy/inc/assets/img/product/clean.svg" alt="" width="20"
+				class="img-fluid mr-2 mb-2">For water based lubricant only!
+		</li>
+		<li class="mb-2">
+			<img src="/wp-content/themes/handy/inc/assets/img/product/safe.svg" alt="" width="20" class="mr-2 mb-2">100% Safe
+			and Anonymous
+		</li>
+		<li class="mb-2">
+			<img src="/wp-content/themes/handy/inc/assets/img/product/rating.svg" alt="" width="20" class="mr-2 mb-2">4.5
+			Customer Rating
+		</li>
+	</ul>
+	<?php endif; ?>
+	<!-- Sleeve specifics accordions -->
+	<?php if (is_product() && has_term( 'soft', 'product_tag' )): ?>
+		<div class="accordion">
+			<div class="accordion-item">
+				<button id="accordion-button-5" aria-expanded="false">
+					<span class="accordion-title">Features</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="features">
+						<li>Inner patterns mimic the sensation of the vagina. </li>
+						<li>One end of the soft sleeve has a different “bobble” pattern. When placed at the base on your penis, it
+							gives a
+							little squeeze which can help you last longer.</li>
+						<li>Placing this bobble pattern near the top of your penis brushes against the glans, for a different
+							sensation.
+						</li>
+						<li>This sleeve is recommended for those looking for a softer, stretchier feeling.</li>
+					</ul>
 				</div>
 			</div>
-		</div> -->
+			<div class="accordion-item">
+				<button aria-expanded="false"><span class="accordion-title">Care</span><span class="icon"
+						aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="care">
+						<li>Clean your sleeve directly after use.</li>
+						<li>Use soap and water when you clean the sleeve.</li>
+						<li>You can turn the sleeve inside out for a deeper clean.</li>
+						<li>Dry with a towel or leave it on a clean surface to dry naturally.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Specifications</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="specs">
+						<li>Material: TPE</li>
+						<li>Length: 120 - 130mm (4.7” - 5.1”)</li>
+						<li>Diameter: 15 – 80mm (0.59” - 3.14”)</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+	<?php if (is_product() && has_term( 'medium', 'product_tag' )): ?>
+		<div class="accordion">
+			<div class="accordion-item">
+				<button id="accordion-button-5" aria-expanded="false">
+					<span class="accordion-title">Features</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="features">
+						<li>Different inner patterns create a snug and secured sensation from one end.</li>
+						<li>Using it from another end, the wavey patterns stimulates the frenulum and glans of the penis.</li>
+						<li>This sleeve is recommended for those looking for a moderately snug feeling. 
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Care</span>
+					<span class="icon"
+						aria-hidden="true">
+					</span>
+				</button>
+				<div class="accordion-content">
+					<ul class="care">
+						<li>Clean your sleeve directly after use.</li>
+						<li>Use soap and water when you clean the sleeve.</li>
+						<li>You can turn the sleeve inside out for a deeper clean.</li>
+						<li>Dry with a towel or leave it on a clean surface to dry naturally.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Specifications</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="specs">
+						<li>Material: TPE</li>
+						<li>Length: 120 - 130mm (4.7” - 5.1”)</li>
+						<li>Diameter: 13 – 60mm (0.51” - 2.36”)</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+	<?php if (is_product() && has_term( 'hard', 'product_tag' )): ?>
+		<div class="accordion">
+			<div class="accordion-item">
+				<button id="accordion-button-5" aria-expanded="false">
+					<span class="accordion-title">Features</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="features">
+						<li>Feel the extreme, the firm tight squeeze of the hard version. The firmness of this sleeve makes it the least stretchy and adaptable to your body of the Open-Ended Collection, so we recommend this firmness only for fans of a very snug experience.</li>
+						<li>Criss-crossed inner patterns on one end mimics a licking sensation and you will feel it more on your frenulum.</li>
+						<li>Using it from the other end with the bobbly bumps may provide a sensation that mimics that of an anal experience.</li>
+						<li>This sleeve is highly recommended for those looking for a super snug, super tight feeling. <b>Using a lubricant is highly recommended.</b></li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Care</span>
+					<span class="icon"
+						aria-hidden="true">
+					</span>
+				</button>
+				<div class="accordion-content">
+					<ul class="care">
+						<li>Clean your sleeve directly after use.</li>
+						<li>Use soap and water when you clean the sleeve.</li>
+						<li>You can turn the sleeve inside out for a deeper clean.</li>
+						<li>Dry with a towel or leave it on a clean surface to dry naturally.</li>
+						<li>Place sleeve back in the foil bag until your next use.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Specifications</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="specs">
+						<li>Material: TPE</li>
+						<li>Length: 120 - 130mm (4.7” - 5.1”)</li>
+						<li>Diameter: 11 – 50mm (0.43” - 1.9”)</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+	<?php if (is_product() && has_term( 'true-grip', 'product_tag' )): ?>
+		<div class="accordion">
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Care</span>
+					<span class="icon"
+						aria-hidden="true">
+					</span>
+				</button>
+				<div class="accordion-content">
+					<ul class="care">
+						<li>Clean your sleeve directly after use.</li>
+						<li>Dry with a towel or leave it on a clean surface to dry naturally.</li>
+						<li>Place sleeve back in the foil bag until your next use.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-item">
+				<button aria-expanded="false">
+					<span class="accordion-title">Specifications</span><span class="icon" aria-hidden="true"></span></button>
+				<div class="accordion-content">
+					<ul class="specs">
+						<li>Material: TPE</li>
+						<li>Length: 120 - 130mm (4.7” - 5.1”)</li>
+						<li>Diameter: 22 – 90mm (0.8” - 3.5”)</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	<?php endif; ?>
 	<!-- Handy Specifics USPs & Specs -->
 	<?php if (is_product() && has_term( 'handy', 'product_tag' )): ?>
@@ -170,6 +274,5 @@ if ( ! $short_description ) {
 			</div>
 		</div>
 	</div>
-
 	<?php endif; ?>
 </div>
